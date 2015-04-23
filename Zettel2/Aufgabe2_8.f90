aad769e434977fbb9897373efb1a24f94e6bb02b
Simonjohanning <Simonjohanning@gmail.com>
2015-04-23 12:47:24 +0200

    Verbesserung des Algos

--- Zettel2/Aufgabe2_8.f90
+++ Zettel2/Aufgabe2_8.f90
@@ -1,54 +1,44 @@
 program Aufgabe2_8
   implicit none
-
   !variable declaration
   integer :: x
-
+  write(*,*) '------------------'
+  write(*,*) 'Verhalten mittels unserer Lösung:'
+  write(*,*) '------------------'
   do x=1,19,2
-	 call naive_exp(dble(x))
-	 call naive_exp(dble(-x))
+	  call improved(dble(x))
+	  call improved(dble(-x))
   end do
-
-
-
 end program Aufgabe2_8
 
-
-subroutine naive_exp(x)
+subroutine improved(x)
   implicit none
-  double precision :: partialsum, partialsum_previous, x_pow, x
-  integer :: k, k_factorial
+  double precision :: partialsum, partialsum_previous, summand_previous, x
+  integer :: k
   logical :: result_change
   !setup variables
-  x_pow = 1.
   result_change = .TRUE.
   partialsum_previous = 0.
-	!set partial sum to the first element (so it's different)
+ 	!set partial sum to the first element (so it's different)
   partialsum = 1.
   	!start with the second element (since first is impicitely computed already)
   k = 1
-  k_factorial = 1
-  !Calculate exp naively
+  summand_previous = 1.
   do while(result_change)
-   	partialsum_previous = partialsum
-	x_pow = dble(x) * x_pow
-	k_factorial = k*k_factorial
-	partialsum = partialsum + (x_pow / dble(k_factorial))
-	write(*,*) 'Partialsumme ',partialsum_previous,' + ',(x_pow / dble(k_factorial))
-	write(*,*) ' ergibt neue Partialsumme ',partialsum
+	partialsum_previous = partialsum
+	partialsum = partialsum_previous + (summand_previous * (dble(x) / dble(k)))
+!	write(*,*) 'Partialsumme ',partialsum_previous,' + ',(summand_previous * (dble(x) / dble(k)))
+!	write(*,*) ' ergibt neue Partialsumme ',partialsum
 !	write(*,*) 'partialsum = ',partialsum,' partialsum_previous = ',partialsum_previous
 !	write(*,*) 'partialsum == partialsum_previous = ',partialsum == partialsum_previous
 	result_change = .NOT. (partialsum == partialsum_previous)	
-	if(ISNAN(partialsum)) result_change = .FALSE.
+	summand_previous = summand_previous * (dble(x) / dble(k))
 	k = k+1
-	!if(k > 10) result_change = .FALSE.
-	!write(*,*) 'x_pow ist ',x_pow
-  end do	
+  end do
   write(*,*) 'Programm hat nach ',k,' Iterationsschritten mit dem Ergebnis ',partialsum,' für x = ',x,'terminiert.'
   write(*,*) 'Eingebaute e-Funktion liefert ',exp(dble(x))
- ! return
-end subroutine naive_exp
-
-
-
-
+!  write(*,'(A,E16.16)') 'Eingebaute e-Funktion liefert ',exp(dble(x))
+	write(*,*) 'Der relative Fehler beträgt:',((partialsum - exp(dble(x)))/exp(dble(x)))
+	write(*,*)
+!10 format (2(A100, E16.15))
+end subroutine improved
--- Zettel2/Aufgabe2_8_improved.f90
+++ Zettel2/Aufgabe2_8_improved.f90
@@ -6,12 +6,12 @@ program Aufgabe2_8
   write(*,*) 'Verhalten mittels unserer Lösung:'
   write(*,*) '------------------'
   do x=1,19,2
-	  call improved(dble(x))
+	  call poor(dble(x))
 	  call improved(dble(-x))
   end do
 end program Aufgabe2_8
 
-subroutine improved(x)
+subroutine poor(x)
   implicit none
   double precision :: partialsum, partialsum_previous, summand_previous, x
   integer :: k
@@ -27,8 +27,8 @@ subroutine improved(x)
   do while(result_change)
 	partialsum_previous = partialsum
 	partialsum = partialsum_previous + (summand_previous * (dble(x) / dble(k)))
-	write(*,*) 'Partialsumme ',partialsum_previous,' + ',(summand_previous * (dble(x) / dble(k)))
-	write(*,*) ' ergibt neue Partialsumme ',partialsum
+!	write(*,*) 'Partialsumme ',partialsum_previous,' + ',(summand_previous * (dble(x) / dble(k)))
+!	write(*,*) ' ergibt neue Partialsumme ',partialsum
 !	write(*,*) 'partialsum = ',partialsum,' partialsum_previous = ',partialsum_previous
 !	write(*,*) 'partialsum == partialsum_previous = ',partialsum == partialsum_previous
 	result_change = .NOT. (partialsum == partialsum_previous)	
@@ -37,4 +37,41 @@ subroutine improved(x)
   end do
   write(*,*) 'Programm hat nach ',k,' Iterationsschritten mit dem Ergebnis ',partialsum,' für x = ',x,'terminiert.'
   write(*,*) 'Eingebaute e-Funktion liefert ',exp(dble(x))
+!  write(*,'(A,E16.16)') 'Eingebaute e-Funktion liefert ',exp(dble(x))
+	write(*,*) 'Der relative Fehler beträgt:',((partialsum - exp(dble(x)))/exp(dble(x)))
+	write(*,*)
+!10 format (2(A100, E16.15))
+end subroutine poor
+
+subroutine improved(x)
+	implicit none
+  double precision :: partialsum, partialsum_previous, summand_previous, x
+  integer :: k
+  logical :: result_change
+  !setup variables
+  result_change = .TRUE.
+  partialsum_previous = 0.
+ 	!set partial sum to the first element (so it's different)
+  partialsum = 1.
+  	!start with the second element (since first is impicitely computed already)
+  k = 1
+  summand_previous = 1.
+  do while(result_change)
+	partialsum_previous = partialsum
+	partialsum = partialsum_previous + (summand_previous * (dble(-x) / dble(k)))
+!	write(*,*) 'Partialsumme ',partialsum_previous,' + ',(summand_previous * (dble(x) / dble(k)))
+!	write(*,*) ' ergibt neue Partialsumme ',partialsum
+!	write(*,*) 'partialsum = ',partialsum,' partialsum_previous = ',partialsum_previous
+!	write(*,*) 'partialsum == partialsum_previous = ',partialsum == partialsum_previous
+	result_change = .NOT. (partialsum == partialsum_previous)	
+	summand_previous = summand_previous * (dble(-x) / dble(k))
+	k = k+1
+  end do
+	!invert the result
+	partialsum = dble(1./partialsum)
+  write(*,*) 'Programm hat nach ',k,' Iterationsschritten mit dem Ergebnis ',partialsum,' für x = ',x,'terminiert.'
+  write(*,*) 'Eingebaute e-Funktion liefert ',exp(dble(x))
+!  write(*,'(A,E16.16)') 'Eingebaute e-Funktion liefert ',exp(dble(x))
+	write(*,*) 'Der relative Fehler beträgt:',((partialsum - exp(dble(x)))/exp(dble(x)))
+	write(*,*)
 end subroutine improved
