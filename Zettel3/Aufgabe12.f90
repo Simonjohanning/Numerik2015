6981eec8b8428706189bdb551ce54c6c62ebbb06
Ruth Boersma <ruth.boersma@gmail.com>
2015-04-28 18:17:34 +0200

    Kommentierung Programm Aufgabe12

--- Zettel3/Aufgabe12.f90
+++ Zettel3/Aufgabe12.f90
@@ -6,13 +6,17 @@ implicit none
 	b = 2
 	u = 0.1
 	uApprox = 0.
+	!Ueber Ziffern iterieren
 	do i = 0,23
+		!Ueber Werte der Ziffern iterieren
 		do k = 1,b
 !			write(*,*) 'k=',k
 !			write(*,*) 'real((b-k)/real(b**i))',real((b-k)/real(b**i))
+			!...bis maximale Ziffer gefunden
 			if((uApprox+real((b-k)/real(b**i))) <= u) then
 				uApprox = uApprox+real((b-k)/real(b**i))
 				ziffern(i+1) = b-k
+				!Dann inneren loop abbrechen
 				exit
 			end if
 		end do
